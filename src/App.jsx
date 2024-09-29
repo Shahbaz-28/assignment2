cbf1917d05608e94234fca49b0ff48b0fd2f2197
shahbaz28 <shahbaz.khan2865@gmail.com>
2024-09-30 00:40:54 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import MasterForm from "./components/task1/MasterForm";
+// import MasterForm from "./components/task1/MasterForm";
 import MasterCreateAccount from "./components/task2/MasterCreateAccount";
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<MasterForm />} />
+        {/* <Route path="/" element={<MasterForm />} /> */}
         <Route path="/create" element={<MasterCreateAccount />} />
       </Routes>
     </>
